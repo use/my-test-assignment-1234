2d28ccd6332091d07845eabf574439063a7e8ac3
Will Hall <willhall@outlook.com>
2021-09-30 00:01:03 +0000

    Add starter code and test cases

---
+++ mymath.py
@@ -0,0 +1,5 @@
+def multiply(a: int, b: int):
+    # your code here
+
+def add(a: int, b: int):
+    # your code here
---
+++ test_mymath.py
@@ -0,0 +1,7 @@
+import mymath
+
+def test_multiply():
+    assert mymath.multiply(5, 6) == 30
+
+def test_add():
+    assert mymath.add(8, 11) == 19
